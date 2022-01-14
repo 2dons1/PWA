c00fdc4760b3d889e727acd806889ad6d6260ea2
dorian dons <dorian.doncevic@gmail.com>
2022-01-14 12:27:59 +0100

    Static2

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ express()
     .get('/', function(req, res) {
         res.sendFile(path.join(__dirname, '/index.html'));
     })
-    .use(express.static('public'))
+    .use(express.static('css'))
+    .use(express.static('img'))
+    .use(express.static('js'))
+    .use(express.static('pages'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
