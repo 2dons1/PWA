dd11bc7a3a3e4400758eddcfd77c59be8a2b586f
dorian dons <dorian.doncevic@gmail.com>
2022-01-14 12:47:06 +0100

    Template s predavanja

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3000
 
 express()
-    .get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '/index.html'));
-    })
     .use(express.static(path.join(__dirname, 'css')))
     .use(express.static(path.join(__dirname, 'img')))
     .use(express.static(path.join(__dirname, 'jss')))
     .use(express.static(path.join(__dirname, 'pages')))
+    .get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '/index.html'));
+    })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- js/app.js
+++
@@ -1,6 +0,0 @@
-// Register a service worker.
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
-        .then((reg) => a = 1) // This runs if promise is resolved.
-        .catch((err) => b = 1) // This runs if promise failes.
-}
