ef9b344774160326e98bf17df7ab82fbd24ed64f
dorian dons <dorian.doncevic@gmail.com>
2022-01-14 18:06:24 +0100

    1-4

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,56 @@
-self.addEventListener('install', event => {
-    console.log('Service worker installing…');    
+// App Shell = core design of a website..
+const version = 'v6';
+const appShell = 'site-static' + "-" + version;
+const dynamicCahce = 'site-dynamic' + "-" + version;
+
+const staticAssets = [
+    '/',
+    'manifest.json',
+    'index.html',
+    'app.html',
+    'fallback.html',
+    'about.html',
+    './js/app.js',
+    './assets/site.css',
+    './assets/img/android/android-launchericon-144-144.png'
+];
+
+// Listen for install event!
+self.addEventListener('install', evt => {
+    // Cache basic files(app shell).
+    evt.waitUntil(caches.open(appShell).then(cache => {
+        cache.addAll(staticAssets);
+    }));
 });
 
-self.addEventListener('activate', event => {
-    console.log('Service worker installing…');
+//  Listen for service worker activation!
+self.addEventListener('activate', evt => {
+    // Delete old caches
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== appShell && key !== dynamicCahce)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 });
 
- self.addEventListener('fetch', () => console.log("fetch"));
+// Listen for a fetch event!
+self.addEventListener('fetch', evt => {
+    // If a fetch request is already cached get the result from cache instead of making a request to the server.
+    evt.respondWith(
+        caches.match(evt.request).then(cacheResponse => {
+            return cacheResponse || fetch(evt.request).then(fetchResponse => {
+                return caches.open(dynamicCahce).then(cache => {
+                    cache.put(evt.request.url, fetchResponse.clone());
+                    return fetchResponse;
+                })
+            });
+        }).catch(() => { // If we are offline and .html file is requested send back the fallback.html.
+            if(evt.request.url.indexOf('.html') > - 1){
+                return caches.match('fallback.html');
+            }
+        })
+    );
+});
