e546cc933a134d1514595b251fefa2146f9524a4
dorian dons <dorian.doncevic@gmail.com>
2022-01-14 10:48:13 +0100

    html push

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path');
 const PORT = process.env.PORT || 3000
 
 express()
-    .get('/', function(req, res){
-        res.send("Testiranje Heorku")
+    .get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '/index.html'));
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
