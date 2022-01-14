1f6a9b3c0749a344da5accab4af192746576b59a
dorian dons <dorian.doncevic@gmail.com>
2022-01-14 10:45:53 +0100

    Commit 2

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 3000
 
 express()
     .get('/', function(req, res){
-        res.send("Testiranje")
+        res.send("Testiranje Heorku")
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
