f18a3adfc664ce24f52a26a52075b5cfba77c13e
dorian dons <dorian.doncevic@gmail.com>
2022-01-14 12:49:41 +0100

    Predavanje

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 3000
 
 express()
-    .use(express.static(path.join(__dirname, 'css')))
-    .use(express.static(path.join(__dirname, 'img')))
-    .use(express.static(path.join(__dirname, 'jss')))
-    .use(express.static(path.join(__dirname, 'pages')))
+    .use(express.static(path.join(__dirname, 'public')))
     .get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '/index.html'));
+        res.sendFile(path.join(__dirname, "public", "/index.html"));
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
